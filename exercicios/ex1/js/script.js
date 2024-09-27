719e0a02341c9ceb51eed10daca39d4755f0b7ee
brenkjk1 <breno.slima1@senacsp.edu.br>
2024-09-27 20:34:42 -0300

    feat: Ex2 corrigido

---
+++ exercicios/ex1/js/script.js
@@ -0,0 +1,12 @@
+const Nome = "Breno";
+let Idade = 22;
+const Nascimento = 21.02;
+
+// Forma 1
+console.log(Nome + ", " + Idade + ", " + Nascimento);
+
+// Forma 2
+console.log(Nome + ', '  + Idade + ', ' + Nascimento);
+
+// Forma 3
+console.log(`${Nome}, ${Idade}, ${Nascimento}`);
---
+++ exercicios/ex2/js/script.js
@@ -0,0 +1,18 @@
+let n1 = "1";
+let n2 = "2";
+let n3 = "3";
+let n4 = "4";
+let n5 = "5";
+let n6 = "6";
+
+n1 = parseFloat(n1);
+n2 = parseFloat(n2);
+n3 = parseFloat(n3);
+n4 = parseFloat(n4);
+n5 = parseFloat(n5);
+n6 = parseFloat(n6);
+
+
+let media = (n1+n2+n3+n4+n5+n6) / 6;
+
+console.log(` A média é: ${media}`);
