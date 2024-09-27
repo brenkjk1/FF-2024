c42b2d7e42106568107cf6878b664e6d87c493fe
BRENO SILVA LIMA <breno.slima1@senacsp.edu.br>
2024-09-26 22:09:25 -0300

    Adicionado o código JS

---
+++ FF-2024/js/script.js
@@ -0,0 +1,19 @@
+// Entrada de dados
+let num1 = parseFloat(prompt("Digite o primeiro número"));
+let num2 = parseFloat(prompt("Digite o segundo número"));
+
+// Processamento
+let soma = num1 + num2;
+let subtracao = num1 - num2;
+let multiplicacao = num1 * num2;
+let divisao = num1 / num2;
+let exponenciacao = num1 ** num2;
+let modulo = num1 % num2;
+
+// Saída de dados
+alert("A Soma é: " + soma);
+alert("A Subtração é: " + subtracao);
+alert("A Multiplicação é: " + multiplicacao);
+alert("A Divisão é: " + divisao);
+alert("A Exponenciação de num1 a num2 é: " + exponenciacao);
+alert("O Resto da divisão é: " + modulo);
