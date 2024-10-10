2668fca8c8292a7a0373b2f2c3e5d63c1bc60f88
brenkjk1 <breno.slima1@senacsp.edu.br>
2024-10-10 19:26:19 -0300

    feat: exemplo 4 corrigido

--- exercicios/ex5/js/script.js
+++ exercicios/ex5/js/script.js
@@ -1,5 +1,11 @@
+// function verificarNumero(val) {
+//     return isNaN(valor);    
+// }
+
+// alert(`É uma letra?` ${verificarNumero(5)});
+
 function verificarNumero(val) {
     return isNaN(valor);    
 }
 
-alert(`É uma letra?` ${verificarNumero(5)});
+alert(`É uma letra?` ${verificarNumero(L)});
